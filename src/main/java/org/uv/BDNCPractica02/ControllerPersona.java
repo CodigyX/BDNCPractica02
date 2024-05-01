022cff714c749464d69271ceddac3a856542ab75
CodigyX <erickbg8919@gmail.com>
2024-05-01 17:01:53 -0600

    Create Project and GetMapping List

---
+++ src/main/java/org/uv/BDNCPractica02/ControllerPersona.java
@@ -0,0 +1,50 @@
+package org.uv.BDNCPractica02;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ *
+ * @author Codigy
+ */
+@RestController
+@RequestMapping("/api/v1/")
+public class ControllerPersona {
+    @Autowired
+    private RepositoryPersona repositoryPersona;    
+    
+    @GetMapping("/persona/")
+    public List<Persona> list() {
+        return repositoryPersona.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Object get(@PathVariable String id) {
+        return null;
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Object input) {
+        return null;
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Object input) {
+        return null;
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        return null;
+    }
+    
+}
